5b35687ee12c12babc5176aad5fd29efb216f396
Alessandro Faniuolo <alessandro.faniuolo@velir.com>
2019-04-09 22:02:38 -0400

    Mapped parameter elements for some of the form fields. Implemented new field converters.

---
+++ src/WFFM.ConversionTool.Library/Converters/FieldConverters/DateConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFFM.ConversionTool.Library.Converters.FieldConverters
+{
+	public class DateConverter : BaseFieldConverter
+	{
+		public override string ConvertValue(string sourceValue)
+		{
+			DateTime sourceDate;
+			if (DateTime.TryParseExact(sourceValue, "yyyyMMddThhmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out sourceDate))
+			{
+				return sourceDate.ToUniversalTime().ToString("yyyyMMddThhmmssZ");
+			}
+			return sourceValue;
+		}
+	}
+}
