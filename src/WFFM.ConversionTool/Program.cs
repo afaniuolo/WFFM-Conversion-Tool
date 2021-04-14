1e70b81c997a7d2595a93ec881ed34e6d214a087
Alessandro Faniuolo <alessandro.faniuolo@gmail.com>
2021-04-13 20:28:02 -0400

    Updated tool version to 1.3.4

--- src/WFFM.ConversionTool/Program.cs
+++ src/WFFM.ConversionTool/Program.cs
@@ -62,7 +62,7 @@ namespace WFFM.ConversionTool
 			System.Console.WriteLine();
 			System.Console.WriteLine("  ***********************************************************************");
 			System.Console.WriteLine("  *                                                                     *");
-			System.Console.WriteLine("  *                 WFFM Conversion Tool - v1.3.3                       *");
+			System.Console.WriteLine("  *                 WFFM Conversion Tool - v1.3.4                       *");
 			System.Console.WriteLine("  *                                                                     *");
 			System.Console.WriteLine("  ***********************************************************************");
 			System.Console.WriteLine();
