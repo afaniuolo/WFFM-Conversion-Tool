1125b8cd3f76257206259e4f22d98796dec40bfa
Alessandro Faniuolo <alessandro.faniuolo@gmail.com>
2022-10-07 12:10:54 -0400

    Updated tool version to 1.6.0

--- src/WFFM.ConversionTool/Program.cs
+++ src/WFFM.ConversionTool/Program.cs
@@ -62,7 +62,7 @@ namespace WFFM.ConversionTool
 			System.Console.WriteLine();
 			System.Console.WriteLine("  ***********************************************************************");
 			System.Console.WriteLine("  *                                                                     *");
-			System.Console.WriteLine("  *                 WFFM Conversion Tool - v1.5.0                       *");
+			System.Console.WriteLine("  *                 WFFM Conversion Tool - v1.6.0                       *");
 			System.Console.WriteLine("  *                                                                     *");
 			System.Console.WriteLine("  ***********************************************************************");
 			System.Console.WriteLine();
